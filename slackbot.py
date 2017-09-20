81695980f89e6d5520037a1f3b1b0cc97e3c4540
antho <anthonyabate31@gmail.com>
2017-09-19 22:18:10 -0500

    more details in transaction data

--- slackbot.py
+++ slackbot.py
@@ -26,10 +26,26 @@ transactions = json_response['query']['results']['league']['transactions']['tran
 for team in standings['standings']['teams']['team']:
 	print json.dumps(team['name'],sort_keys=True, indent=4, separators=(',', ': '))
 
-for transaction in transactions:
-	print transaction['type']
+for transaction in transactions[:10]:
+	type = transaction['type']
 	try:
-		print transaction['players']['player']['name']
+		if type == 'add' or type == 'drop':
+			print type
+			print "Player: {}".format(transaction['players']['player']['name']['full'])
+			print "From: {}".format(transaction['players']['player']['transaction_data']['source_team_name'])
+			print "To: {}".format(transaction['players']['player']['transaction_data']['destination_type'])
+		else:
+			for players in transaction['players']['player']:
+				if players['transaction_data']['type'] == "add":
+					print players['transaction_data']['type']
+					print "Player: {}".format(players['name']['full'])
+					print "From: {}".format(players['transaction_data']['destination_team_name'])
+					print "To: {}".format(players['transaction_data']['source_type'])
+				else:
+					print players['transaction_data']['type']
+					print "Player: {}".format(players['name']['full'])
+					print "From: {}".format(players['transaction_data']['source_team_name'])
+					print "To: {}".format(players['transaction_data']['destination_type'])
 	except TypeError as e:
 		continue
 	except KeyError as e:
